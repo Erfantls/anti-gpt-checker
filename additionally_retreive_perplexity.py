635ac6d54a9d9abeb37b6692173613c586bc8fdc
Paweł Gryka <pawel.m.gryka@gmail.com>
2024-12-09 23:04:34 +0100

    Add suppression of output for perplexity calculation

--- additionally_retreive_perplexity.py
+++ additionally_retreive_perplexity.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     for attribute_in_db in tqdm(not_processed_perplexity, total=len(not_processed_perplexity),
                                 desc=f'Calculating perplexity of lab reports', unit='Lab reports', miniters=1):
         text_to_analyse = attribute_in_db.stylometrix_metrics.text
-        #with suppress_stdout():
-        perplexity_base, perplexity = calculate_perplexity(text_to_analyse, 'pl', return_both=True, force_use_cpu=True)
+        with suppress_stdout():
+            perplexity_base, perplexity = calculate_perplexity(text_to_analyse, 'pl', return_both=True, force_use_cpu=True)
         dao_attributes.update_one({'_id': attribute_in_db.id},
                                   {"$set": {'perplexity_base': perplexity_base, 'perplexity': perplexity}})
