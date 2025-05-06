de887562bb5c5402ab96184034c81685a6793c87
mksochota16 <pawel.m.gryka@gmail.com>
2025-05-06 15:20:12 +0200

    Add framework for API

---
+++ api/models/response.py
@@ -0,0 +1,64 @@
+from enum import Enum
+from typing import Union, Optional, List, Literal
+
+from pydantic import BaseModel
+
+from models.attribute import PartialAttribute, AttributeInDB
+from models.base_mongo_model import MongoObjectId
+
+
+class NoReviewsFoundResponse(BaseModel):
+    type = "no_reviews_found_response"
+    message: str = "No reviews found on the specified place"
+
+class FailedToExtractData(BaseModel):
+    type = "failed_to_extract_data_response"
+    message: str = "Failed to extract attributes from the given document, if you are sure that the document is not malformed, please call renew-markers endpoint first"
+
+class BackgroundTaskStillRunningResponse(BaseModel):
+    type = "background_task_still_running_response"
+    message: str = "Background task is still running, please wait the given wait-time and then call this endpoint again"
+    document_id: str
+    estimated_wait_time: int
+    analysis_id = None
+
+class BackgroundTaskRunningResponse(BaseModel):
+    type = "background_task_running_response"
+    message: str = "Background task is running, please wait the given wait-time and call check-results endpoint with the given document_id"
+    document_id: str
+    estimated_wait_time: int
+    analysis_id = None
+
+class BackgroundTaskFinishedResponse(BaseModel):
+    type = "background_task_finished_response"
+    message: str = "Background task is finished"
+    document_id: str
+    estimated_wait_time: int = 0
+    analysis_id: MongoObjectId
+
+BackgroundTaskStatusResponse = Union[BackgroundTaskRunningResponse, BackgroundTaskFinishedResponse, BackgroundTaskStillRunningResponse]
+
+class AnalysisData(BaseModel):
+    analysis_id: MongoObjectId
+    document_id: str
+    full_features: AttributeInDB
+    partial_features: Optional[List[PartialAttribute]] = None
+
+class AnalysisResultsResponse(BaseModel):
+    type = "analysis_results_response"
+    message: str = "Analysis results"
+    analysis_data: AnalysisData
+
+class LightbulbScoreType(str, Enum):
+    BIDIRECTIONAL = "bidirectional" # score [-1,1]
+    HUMAN_WRITTEN = "human_written" # score [-1,0]
+    LLM_GENERATED = "llm_generated" # score [0,1]
+
+class LightbulbScoreData(BaseModel):
+    attribute_name: str
+    type: LightbulbScoreType
+    score: float
+
+class LightbulbScoreResponse(BaseModel):
+    type = "lightbulb_score_response"
+    lightbulb_scores: List[LightbulbScoreData]
