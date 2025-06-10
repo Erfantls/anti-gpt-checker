d79916097ece74e7e0655f291c7075119c768a06
mksochota16 <pawel.m.gryka@gmail.com>
2025-06-10 20:26:43 +0200

    Add llm model name field to LabReport

--- models/lab_report.py
+++ models/lab_report.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import BaseModel
 
 from models.base_mongo_model import MongoDBModel
@@ -6,6 +8,7 @@ class LabReport(BaseModel):
     plaintext_content: str
     is_generated: bool
     tag: str
+    model: Optional[str]
 
 
 class LabReportInDB(MongoDBModel, LabReport):
