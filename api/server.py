933cfd8e0a430c04ef66abc491d0343595aeb2a5
mksochota16 <pawel.m.gryka@gmail.com>
2025-06-10 21:45:13 +0200

    Change server port

--- api/server.py
+++ api/server.py
@@ -24,6 +24,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "api.server:app",       # points to this file and the FastAPI instance
         host="0.0.0.0",
-        port=8000,
+        port=8989,
         reload=True,      # auto reload during development
     )
