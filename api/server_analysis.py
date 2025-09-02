3bbf11c6357499584a684cea8bcbd47dce1797a4
mksochota16 <pawel.m.gryka@gmail.com>
2025-09-02 15:26:01 +0200

    Fix 2 reference attributes not being downloaded in the init of analysis server

--- api/server_analysis.py
+++ api/server_analysis.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-from api.analyser import load_reference_attributes
+from api.analyser import load_reference_attributes, precompile_gaussian_kde
 from api.feature_extraction import router as feature_extraction_router, init_analysis_executor
 from api.server_config import init_analysis_task_queue
 
@@ -29,6 +29,10 @@ async def lifespan(app: FastAPI):
     load_reference_attributes()
     print("Reference attributes loaded successfully.")
     print("=========================================================")
+
+    print("Precompile gaussian kde values...")
+    precompile_gaussian_kde()
+    print("Precompilation of gaussian kde values completed successfully.")
     print("=========================================================")
 
     yield
