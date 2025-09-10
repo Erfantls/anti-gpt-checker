c177c2f09133767e8eddf1c2e43e75d28c3dc6fc
mksochota16 <pawel.m.gryka@gmail.com>
2025-09-10 19:51:18 +0200

    Test bug fix

--- api/feature_extraction.py
+++ api/feature_extraction.py
@@ -247,7 +247,7 @@ def _blocking_analysis(analysis_id: str, document_hash, user_id: str, type_of_an
             lightbulb_combination_features_scores = calculate_lightbulb_scores(attribute_after_update, [attribute for attribute in API_MOST_IMPORTANT_ATTRIBUTES if 'combination_features' in attribute])
             set_query_dict = {}
             for combination_feature_score in lightbulb_combination_features_scores:
-                set_query_dict[f"lightbulb_scores_dict.{combination_feature_score.attribute_name}"] = combination_feature_score.dict()
+                set_query_dict[f"lightbulb_scores_dict.{combination_feature_score.attribute_name}.test"] = combination_feature_score.dict()
 
             dao_lightbulb.update_one({'attribute_id': document_level_attributes_id, 'is_chunk_attribute': False},
                                      {'$set': set_query_dict}
